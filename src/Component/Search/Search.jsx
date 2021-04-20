0f43c11b17289fd08a89f59ebc01b3c1ddd82908
Tuan 2 Nguyen Thanh <v3210106@transcosmos-vn.local>
2021-04-20 17:01:41 +0700

    search

--- src/Component/Search/Search.jsx
+++ src/Component/Search/Search.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react'; 
+import React, { useRef, useState } from 'react'; 
 import SearchIcon from '@material-ui/icons/Search';
 import './Search.scss'
+import { useHistory } from 'react-router';
 
 function Search() {
     const [search, setSearch] = useState([]);
     const [searchBox, setsearchBox] = useState(false);
+    const inputEl = useRef();
+    let history = useHistory();
     const handleChangeInput = async (e) =>{    
         const value = e.target.value;
         if(value !== ''){
@@ -21,13 +24,15 @@ function Search() {
         }
         setsearchBox(false);
     } 
-    const handleClickSearch = (data) =>{
-        console.log("data", data);
+    const handleClickSearch = (data) =>{ 
+        history.push(`detail${data}`) 
+        inputEl.current.value = "";
+        setsearchBox(false)
     }
     return (
         <>
             <div className="search">
-                <input onChange={handleChangeInput} type="text" placeholder="Search movie"/>
+                <input onChange={handleChangeInput} ref={inputEl} type="text" placeholder="Search movie"/>
                 <SearchIcon className="iconSearch" color="primary" fontSize="small"/> 
                 <div style={searchBox ? {display: 'block'} : {display: 'none'} } className="box">
                     <ul>
